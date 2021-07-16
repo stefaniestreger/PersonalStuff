eaf750a490a86342dcb042b028a166493ca60463
Stef Streger <38088922+stefaniestreger@users.noreply.github.com>
2021-07-16 08:57:58 -0500

    Update streger_stefanie_assign11.py

--- streger_stefanie_assign11.py
+++ streger_stefanie_assign11.py
@@ -1,3 +1,6 @@
+# For any list of numbers, if numbers within range 0-100, add to new list valid_numbers.
+# Display valid_numbers, total valid_numbers and average valid_numbers
+
 numbers = [23, 5, 89, 123, -56, 93, 24, 11, -9, 52, 685]
 valid_numbers = []
 for n in numbers:
