3406549d9453febc2a2136c8e84c0409ab0670de
Stef Streger <38088922+stefaniestreger@users.noreply.github.com>
2021-07-16 21:13:38 -0500

    Add files via upload
    Display 3 favorite movies in a text file

---
+++ streger_stefanie_lab12.py
@@ -0,0 +1,8 @@
+def main():
+    movie_list = open('movies.txt', 'w')
+    my_list = ["The Shawshank Redemption", "Urban Cowboy", "What We Do In The Shadows"]
+    for x in my_list:
+        movie_list.write(x + "\n")
+    movie_list.close()
+
+main()
